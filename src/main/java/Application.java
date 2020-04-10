f996f31a125f9064ee7bca667738b0f206e3b1c5
ordinCode <ysh1346@naver.com>
2020-04-10 13:31:27 +0900

    feat : ChickenController
     - 종료를 입력하지 않으면 프로그램 반복

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -16,7 +16,7 @@ public class Application {
         final List<Table> tables = TableRepository.tables();
         OutputView.printTables(tables);
 
-        final int tableNumber = InputView.inputTableNumber();
+        final String tableNumber = InputView.inputTableNumber();
 
         final List<Menu> menus = MenuRepository.menus();
         OutputView.printMenus(menus);
--- src/main/java/controller/ChickenController.java
+++ src/main/java/controller/ChickenController.java
@@ -3,7 +3,16 @@ package controller;
 import view.InputView;
 
 public class ChickenController {
+	private static final String EXIT_FUNCTION_NUMBER = "3";
+
 	public static void run() {
-		InputView.inputAction();
+		String userInput = InputView.inputAction();
+		while (isNotExit(userInput)) {
+
+		}
+	}
+
+	public static boolean isNotExit(final String userInput) {
+		return !EXIT_FUNCTION_NUMBER.equals(userInput);
 	}
 }
